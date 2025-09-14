3c3ff84c14c161e469e9fbffe87e94be30558947
rhpatel2004 <rhpatel2004@gmail.com>
2025-09-14 11:09:52 +0530

    dummy login register page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Login Page</h1>
+    </div>
+  );
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Register Page</h1>
+    </div>
+  );
+}
